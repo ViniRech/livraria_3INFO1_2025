82388a4be2b1aea8b93a0295ecee1ded87b290f1
ViniRech <viniciusrech2007@gmail.com>
2025-05-13 16:06:45 -0300

    feat: criação de dois serializadores para Livro

--- core/models/autor.py
+++ core/models/autor.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Autor(models.Model):
+    class Meta:
+        verbose_name_plural = "autores"
+
     nome = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
 
