cf931073d713e2c362c4897428d70061d665090a
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-07-08 15:58:29 -0300

    feat: criação do endpoint para a listagem básica de compras

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,4 +2,5 @@ from .user import UserSerializer
 from .categoria import CategoriaSerializer
 from .editora import EditoraSerializer
 from .autor import AutorSerializer
+from .compra import CompraSerializer
 from .livro import LivroSerializer, LivroListSerializer, LivroRetrieveSerializer
